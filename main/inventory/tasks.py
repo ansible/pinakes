1c9521f6f139046b9e57877d414a7230e63f0308
Madhu Kanoor <mkanoor@redhat.com>
2021-07-27 15:46:48 -0400

    Changed print messages to log info

--- main/inventory/tasks.py
+++ main/inventory/tasks.py
@@ -1,16 +1,23 @@
 """ Background tasks for inventory """
+import logging
 from main.inventory.task_utils.refresh_inventory import RefreshInventory
 from main.inventory.task_utils.launch_job import LaunchJob
 
+logger = logging.getLogger("inventory")
+
 def refresh_task(tenant_id, source_id):
     """Run the Refresh task"""
+    logger.info("Starting Inventory Refresh")
     obj = RefreshInventory(tenant_id, source_id)
-    return obj.process()
+    result = obj.process()
+    logger.info("Finished Inventory Refresh")
+    return result
 
 
 def launch_tower_task(slug, body):
     """Launch a job on the tower"""
-    print("Launching job")
+    logger.ingo("Launching job")
     obj = LaunchJob(slug, body).process()
-    print(obj)
-    print("finished")
+    logger.info(obj)
+    logger.info("Job finished")
+    return obj
