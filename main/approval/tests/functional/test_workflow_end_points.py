ea4574a3206319d8cebf5c609d37fdccb0108000
Bill Wei <bilwei@redhat.com>
2021-09-02 16:25:22 -0400

    Add tests for ordering, search, and filtering

--- main/approval/tests/functional/test_workflow_end_points.py
+++ main/approval/tests/functional/test_workflow_end_points.py
@@ -18,6 +18,63 @@ def test_workflow_list(api_request):
 
     assert content["count"] == 1
 
+@pytest.mark.django_db
+def test_searching(api_request):
+    """Search by query parameter"""
+    WorkflowFactory(name="alpha", description="hello")
+    WorkflowFactory(name="beta", description="world")
+    url = reverse("workflow-list")
+    response = api_request("get", url, {"search": "eta"})
+    content = json.loads(response.content)
+    assert content["count"] == 1
+    assert content["results"][0]["name"] == "beta"
+
+    response = api_request("get", url, {"search": "l"})
+    content = json.loads(response.content)
+    assert content["count"] == 2
+
+    response = api_request("get", url, {"search": "xyz"})
+    content = json.loads(response.content)
+    assert content["count"] == 0
+
+
+@pytest.mark.django_db
+def test_filtering(api_request):
+    """Filter by query parameter"""
+    WorkflowFactory(name="alpha", description="hello")
+    WorkflowFactory(name="beta", description="world")
+    url = reverse("workflow-list")
+    response = api_request("get", url, {"name": "beta", "description": "world"})
+    content = json.loads(response.content)
+    assert content["count"] == 1
+    assert content["results"][0]["name"] == "beta"
+
+    response = api_request("get", url, {"name": "bet"})
+    content = json.loads(response.content)
+    assert content["count"] == 0
+
+
+@pytest.mark.django_db
+def test_ordering(api_request):
+    """Filter by query parameter"""
+    WorkflowFactory(name="alpha", description="hello")
+    WorkflowFactory(name="beta", description="world")
+    url = reverse("workflow-list")
+    response = api_request("get", url)
+    content = json.loads(response.content)
+    assert content["results"][0]["name"] == "alpha"
+    assert content["results"][1]["name"] == "beta"
+
+    response = api_request("get", url, {"ordering": "name"})
+    content = json.loads(response.content)
+    assert content["results"][0]["name"] == "alpha"
+    assert content["results"][1]["name"] == "beta"
+
+    response = api_request("get", url, {"ordering": "-name"})
+    content = json.loads(response.content)
+    assert content["results"][0]["name"] == "beta"
+    assert content["results"][1]["name"] == "alpha"
+
 
 @pytest.mark.django_db
 def test_workflow_retrieve(api_request):
