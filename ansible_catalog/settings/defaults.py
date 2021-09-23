0fc817532f1c5aaf06cc9af65f8643e21be4d260
Madhu Kanoor <mkanoor@redhat.com>
2021-09-23 10:38:58 -0400

    Fixed PR comments

--- ansible_catalog/settings/defaults.py
+++ ansible_catalog/settings/defaults.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
     "taggit",
     "django_rq",
     "drf_spectacular",
-    "ansible_catalog.main",
     "social_django",
+    "ansible_catalog.main",
 ]
 
 MIDDLEWARE = [
@@ -314,24 +314,4 @@ SPECTACULAR_SETTINGS = {
     ],
 }
 
-SOCIAL_AUTH_POSTGRES_JSONFIELD = True
-SOCIAL_AUTH_JSONFIELD_CUSTOM = "django.contrib.postgres.fields.JSONField"
-SOCIAL_AUTH_URL_NAMESPACE = "social"
-
-SOCIAL_AUTH_PIPELINE = (
-    "social_core.pipeline.social_auth.social_details",
-    "social_core.pipeline.social_auth.social_uid",
-    "social_core.pipeline.social_auth.social_user",
-    "social_core.pipeline.user.get_username",
-    "social_core.pipeline.social_auth.associate_by_email",
-    "social_core.pipeline.user.create_user",
-    "social_core.pipeline.social_auth.associate_user",
-    "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
-)
-
-SOCIAL_AUTH_KEYCLOAK_KEY = "catalog"
-SOCIAL_AUTH_KEYCLOAK_SECRET = "<<YourSecret>>"
-SOCIAL_AUTH_KEYCLOAK_PUBLIC_KEY = "<<YourPublicKey>>"
-SOCIAL_AUTH_KEYCLOAK_AUTHORIZATION_URL = "<<auth_url>>"
-SOCIAL_AUTH_KEYCLOAK_ACCESS_TOKEN_URL = "<<token_url>>"
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
