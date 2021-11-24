83d401599b357c889ddabb30bd9fb3eb6d789f48
Alexander Saprykin <cutwatercore@gmail.com>
2021-11-24 23:54:27 +0100

    Add Keycloak Policy API client

--- ansible_catalog/common/auth/keycloak/models.py
+++ ansible_catalog/common/auth/keycloak/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import Dict, List, Optional, Sequence, Union
 
 import pydantic
 
@@ -9,6 +9,16 @@ from .utils import to_lower_camel
 
 class OpenIDConfiguration(pydantic.BaseModel):
     token_endpoint: str
+
+
+class Uma2Configuration(pydantic.BaseModel):
+    authorization_endpoint: str
+    token_endpoint: str
+    introspection_endpoint: str
+    registration_endpoint: str
+    resource_registration_endpoint: str
+    permission_endpoint: str
+    policy_endpoint: str
 
 
 class ApiModelBase(pydantic.BaseModel):
@@ -31,3 +41,56 @@ class Group(ApiModelBase):
 
 
 Group.update_forward_refs()
+
+
+class Scope(pydantic.BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+
+
+class ResourceOwner(pydantic.BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+
+
+class Resource(pydantic.BaseModel):
+    id: Optional[str] = pydantic.Field(None, alias="_id")
+    name: Optional[str] = None
+    uris: Optional[List[str]] = None
+    type: Optional[str] = None
+    scopes: Optional[List[Scope]] = None
+    icon_uri: Optional[str] = None
+    owner: Optional[ResourceOwner] = None
+    owner_managed_access: Optional[bool] = None
+    display_name: Optional[str] = None
+    attributes: Optional[Dict[str, List[str]]] = None
+
+    class Config:
+        alias_generator = to_lower_camel
+        allow_population_by_field_name = True
+
+    @pydantic.validator("owner", pre=True)
+    def parse_owner(cls, value: Union[str, ResourceOwner]):
+        if isinstance(value, str):
+            value = ResourceOwner(id=value)
+        return value
+
+    @pydantic.validator("scopes", pre=True)
+    def parse_scopes(cls, value: Sequence[Union[str, Scope]]):
+        scopes = []
+        for item in value:
+            if isinstance(item, str):
+                item = Scope(name=item)
+            scopes.append(item)
+        return scopes
+
+
+class UmaPermission(pydantic.BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    roles: Optional[List[str]] = None
+    groups: Optional[List[str]] = None
+    clients: Optional[List[str]] = None
+    users: Optional[List[str]] = None
+    scopes: Optional[List[str]] = None
---
+++ ansible_catalog/common/auth/keycloak/uma.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from . import constants, exceptions
+from . import models
+from . import openid
+from .client import ApiClient
+
+
+class UmaClient:
+    def __init__(self, server_url: str, realm: str, token: str):
+        self._server_url = server_url.rstrip("/")
+        self._realm = realm
+
+        self._uma2_configuration = None
+
+        self._client = ApiClient(token=token)
+
+    def uma2_configuration(
+        self, force_reload=False
+    ) -> models.Uma2Configuration:
+        if self._uma2_configuration is None or force_reload:
+            path = constants.UMA2_CONFIGURATION_PATH.format(realm=self._realm)
+            data = self._client.request_json(
+                "GET", f"{self._server_url}/{path}"
+            )
+            self._uma2_configuration = models.Uma2Configuration.parse_obj(data)
+        return self._uma2_configuration
+
+    # Protection / Resource API
+    def create_resource(self, resource: models.Resource) -> models.Resource:
+        url = self.uma2_configuration().resource_registration_endpoint
+        data = resource.dict(by_alias=True, exclude_unset=True)
+        result = self._client.request_json("POST", url, json=data)
+        return models.Resource.parse_obj(result)
+
+    def update_resource(self, resource: models.Resource) -> None:
+        url = "{endpoint}/{resource_id}".format(
+            endpoint=self.uma2_configuration().resource_registration_endpoint,
+            resource_id=resource.id,
+        )
+        data = resource.dict(by_alias=True, exclude_unset=True)
+        self._client.request("PUT", url, json=data)
+
+    def delete_resource(self, resource_id: str) -> None:
+        url = "{endpoint}/{resource_id}".format(
+            endpoint=self.uma2_configuration().resource_registration_endpoint,
+            resource_id=resource_id,
+        )
+        self._client.request("DELETE", url)
+
+    def get_resource_by_id(self, resource_id: str):
+        url = "{endpoint}/{resource_id}".format(
+            endpoint=self.uma2_configuration().resource_registration_endpoint,
+            resource_id=resource_id,
+        )
+        result = self._client.request_json("GET", url)
+        return models.Resource.parse_obj(result)
+
+    def create_permission(
+        self, resource_id: str, permission: models.UmaPermission
+    ) -> models.UmaPermission:
+        url = "{endpoint}/{resource_id}".format(
+            endpoint=self.uma2_configuration().policy_endpoint,
+            resource_id=resource_id,
+        )
+        data = permission.dict(by_alias=True, exclude_unset=True)
+        result = self._client.request_json("POST", url, json=data)
+        return models.UmaPermission(**result)
+
+    def update_permission(self, permission: models.UmaPermission) -> None:
+        url = "{endpoint}/{permission_id}".format(
+            endpoint=self.uma2_configuration().policy_endpoint,
+            permission_id=permission.id,
+        )
+        data = permission.dict(by_alias=True, exclude_unset=True)
+        self._client.request("PUT", url, json=data)
+
+    def delete_permission(self, permission_id: str) -> None:
+        url = "{endpoint}/{permission_id}".format(
+            endpoint=self.uma2_configuration().policy_endpoint,
+            permission_id=permission_id,
+        )
+        self._client.request("DELETE", url)
+
+    def get_permission_by_name(self, name: str) -> models.UmaPermission:
+        permissions = self._find_permissions(name=name)
+        if not permissions:
+            raise exceptions.NoResultFound
+        if len(permissions) > 1:
+            raise exceptions.MultipleResourcesFound
+        return permissions[0]
+
+    def find_permissions_by_name(
+        self, name: str
+    ) -> List[models.UmaPermission]:
+        return self._find_permissions(name=name)
+
+    def find_permissions_by_resource(
+        self, resource_id: str
+    ) -> List[models.UmaPermission]:
+        return self._find_permissions(resource_id=resource_id)
+
+    def _find_permissions(self, **kwargs) -> List[models.UmaPermission]:
+        url = self.uma2_configuration().policy_endpoint
+        result = self._client.request_json("GET", url, params=kwargs)
+        return [models.UmaPermission.parse_obj(item) for item in result]
+
+
+def create_uma_client(
+    server_url: str,
+    realm: str,
+    client_id: str,
+    client_secret: str,
+) -> UmaClient:
+    oidc_client = openid.OpenIdConnect(
+        server_url, realm, client_id, client_secret
+    )
+    token_info = oidc_client.client_credentials_auth()
+    return UmaClient(server_url, realm, token_info["access_token"])
