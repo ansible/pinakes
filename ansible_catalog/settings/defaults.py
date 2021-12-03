94e9cd493c485a4e349b383327be0bea073f526f
Madhu Kanoor <mkanoor@redhat.com>
2021-12-03 14:48:49 -0500

    Add UI
    First attempt at adding UI for minikube env
    http://catalog.k8s.local/ui/index.html

--- ansible_catalog/settings/defaults.py
+++ ansible_catalog/settings/defaults.py
@@ -167,7 +167,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/ui/"
+STATICFILES_DIRS = [
+    BASE_DIR / "ui",
+    '/ui/',
+]
+LOGIN_REDIRECT_URL="/ui/index.html"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- tools/minikube/scripts/server.sh
+++ tools/minikube/scripts/server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 echo -e "\e[34m >>> Seed Kaycloak data \e[97m"
 ansible-playbook -vvv tools/keycloak_setup/dev.yml
 echo -e "\e[34m >>> Migrating changes \e[97m"
@@ -7,6 +7,11 @@ python manage.py migrate
 echo -e "\e[32m >>> migration completed \e[97m"
 
 echo -e "\e[32m >>> Create Source object\e[97m"
+python manage.py shell < tools/minikube/scripts/initialize_source.py
+
+echo -e "\e[32m >>> Fetch UI tar\e[97m"
+curl -o ui.tar.xz https://raw.githubusercontent.com/lgalis/ansible-catalog-ui-build/main/ui.tar.xz
+tar -xf ui.tar.xz --directory ansible_catalog/ui
 python manage.py shell < tools/minikube/scripts/initialize_source.py
 echo -e "\e[34m >>> Start development server \e[97m"
 python manage.py runserver 0.0.0.0:8000
