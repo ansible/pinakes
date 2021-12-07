7a19dba1638966c41e9234eea2423d48984b6b76
Madhu Kanoor <mkanoor@redhat.com>
2021-12-07 11:56:21 -0500

    Added test file

---
+++ ansible_catalog/main/auth/tests/functional/test_session_logout.py
@@ -0,0 +1,11 @@
+""" Module to test session logout end point """
+import json
+import pytest
+
+
+@pytest.mark.django_db
+def test_session_logout(api_request, mocker):
+    """Logout an authenticated user from a single session"""
+    mocker.patch("ansible_catalog.main.auth.views.AdminClient")
+    response = api_request("post", "logout")
+    assert response.status_code == 200
