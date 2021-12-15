b90085a578e905d205d79c3a60645e19bd4b7f3c
Madhu Kanoor <mkanoor@redhat.com>
2021-12-15 12:59:30 -0500

    Added an optional parameter to delete the persistent volumes

--- tools/minikube/scripts/delete_pods.sh
+++ tools/minikube/scripts/delete_pods.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-pvs=$(kubectl get pv | grep catalog/ | awk '{print $1}')
-kubectl delete namespace catalog
-for pv in "$pvs"
+while getopts dh flag
 do
-    kubectl delete pv $pv
+    case "${flag}" in
+        d) delete_pv=1;;
+        h) echo "usage: delete_pods.sh -d"; exit 0;
+    esac
 done
+
+if [ -n "$delete_pv" ]; then
+	pvs=($(kubectl get pv | grep catalog/ | awk '{print $1}'))
+fi
+
+kubectl delete namespace catalog
+
+if [ -n "$delete_pv" ]; then
+  for pv in "${pvs[@]}"
+  do
+    kubectl delete pv $pv 2> /dev/null
+  done
+fi
