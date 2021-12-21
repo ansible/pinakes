eb931241ee57625e1b4a511f9da716d54d90d316
Alexander Saprykin <cutwatercore@gmail.com>
2021-12-21 13:24:54 +0100

    WIP: Fix mixin class attribute name

--- ansible_catalog/common/auth/keycloak_django/views.py
+++ ansible_catalog/common/auth/keycloak_django/views.py
@@ -1,6 +1,9 @@
-class KeycloakPermissionMixin:
+from rest_framework import views
 
-    keycloak_resource_name = None
+
+class KeycloakPermissionMixin(views.APIView):
+
+    keycloak_resource_type = None
     keycloak_access_policies = {}
 
     def get_keycloak_access_policies(self):
