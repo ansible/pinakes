768b325787c33078e5a395113cd51597264f9247
Alejandro Izquierdo <aizquier@redhat.com>
2022-01-14 17:00:53 +0100

    fix-delete_pods-script

--- tools/minikube/scripts/delete_pods.sh
+++ tools/minikube/scripts/delete_pods.sh
@@ -7,15 +7,21 @@ do
     esac
 done
 
-if [ -n "$delete_pv" ]; then
-	pvs=($(kubectl get pv | grep catalog/ | awk '{print $1}'))
-fi
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/redis-deployment.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/redis-service.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/postgres-deployment.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/postgres-service.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/keycloak-deployment.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/keycloak-service.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/app-deployment.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/app-service.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/ingress.yaml
+kubectl delete --namespace=catalog -f ./tools/minikube/templates/worker-deployment.yaml
 
-kubectl delete namespace catalog
+
 
 if [ -n "$delete_pv" ]; then
-  for pv in "${pvs[@]}"
-  do
-    kubectl delete pv $pv 2> /dev/null
-  done
+    kubectl delete --namespace=catalog -f ./tools/minikube/templates/app-claim0-persistentvolumeclaim.yaml
+    kubectl delete --namespace=catalog -f ./tools/minikube/templates/worker-claim0-persistentvolumeclaim.yaml
+    kubectl delete --namespace=catalog -f ./tools/minikube/templates/pg-data-persistentvolumeclaim.yaml
 fi
