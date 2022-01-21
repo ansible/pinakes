d516992c0788aa4db55e6a4b12850814045d6320
Madhu Kanoor <mkanoor@redhat.com>
2022-01-21 10:34:28 -0500

    Added support for collectstatic
    This collects static files and puts it in a shared location for
    NGINX to use

--- tools/minikube/scripts/server.sh
+++ tools/minikube/scripts/server.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
+if [[ -z "${ANSIBLE_CATALOG_SECRET_KEY}" ]]
+then
+  echo "Please set the environment variable ANSIBLE_CATALOG_SECRET_KEY"
+  exit 1
+fi
+
+if [[ -z "${ANSIBLE_CATALOG_STATIC_ROOT}" ]]
+then
+  echo "Please set the environment variable ANSIBLE_CATALOG_STATIC_ROOT"
+  exit 1
+fi
+
 echo -e "\e[34m >>> Seed Kaycloak data \e[97m"
 ansible-playbook -vvv tools/keycloak_setup/dev.yml
+
 echo -e "\e[34m >>> Migrating changes \e[97m"
 python manage.py migrate
 echo -e "\e[32m >>> migration completed \e[97m"
@@ -10,9 +23,16 @@ echo -e "\e[32m >>> Create Source object\e[97m"
 python manage.py shell < tools/minikube/scripts/initialize_source.py
 
 echo -e "\e[32m >>> Fetch UI tar\e[97m"
-curl -o ui.tar.xz https://raw.githubusercontent.com/lgalis/ansible-catalog-ui-build/main/ui.tar.xz
-mkdir -p /app/ui
-tar -xf ui.tar.xz --directory /app/ui
+
+curl -o ui.tar.xz https://raw.githubusercontent.com/lgalis/ansible-catalog-ui-build/main/catalog_ui.tar.xz
+rm -rf ansible_catalog/ui/catalog
+mkdir -p ansible_catalog/ui/catalog
+tar -xf ui.tar.xz --directory ansible_catalog/ui/catalog
+
+rm -rf "$ANSIBLE_CATALOG_STATIC_ROOT"
+
+echo -e "\e[34m >>> Collect static files \e[97m"
+python manage.py collectstatic
 
 echo -e "\e[34m >>> Start gunicorn server \e[97m"
 #python manage.py runserver 0.0.0.0:8000
