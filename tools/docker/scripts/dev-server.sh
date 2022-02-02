adf9c3b3a31d7a1de7c2756f331f263b2f124532
Alejandro Izquierdo <aizquier@redhat.com>
2022-02-02 17:03:44 +0100

    fix scripts

--- tools/docker/scripts/dev-server.sh
+++ tools/docker/scripts/dev-server.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+set -e
 
 while true; do
     echo -e "\e[34m >>> Waiting for postgres \e[97m"
-    python -c "import socket; socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((\"$ANSIBLE_CATALOG_POSTGRES_HOST\", 5432))" && break
+    if python -c "import socket; socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((\"$ANSIBLE_CATALOG_POSTGRES_HOST\", 5432))"; then
+        echo -e "\e[34m >>> Postgres ready \e[97m"
+        break
+    fi
     sleep 1
 done
 
 echo -e "\e[34m >>> Migrating changes \e[97m"
-python manage.py migrate || exit 1
+python manage.py migrate
 echo -e "\e[32m >>> migration completed \e[97m"
 
 echo -e "\e[34m >>> Collecting static files \e[97m"
-python manage.py collectstatic --no-input || exit 1
+python manage.py collectstatic --no-input
 
 echo -e "\e[34m >>> Start development server \e[97m"
-python manage.py runserver 0.0.0.0:8000 || exit 1
+python manage.py runserver 0.0.0.0:8000
--- tools/docker/scripts/worker.sh
+++ tools/docker/scripts/worker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+
 while true; do
     echo -e "\e[34m >>> Waiting for postgres \e[97m"
-    python -c "import socket; socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((\"$ANSIBLE_CATALOG_POSTGRES_HOST\", 5432))" && break
+    if python -c "import socket; socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((\"$ANSIBLE_CATALOG_POSTGRES_HOST\", 5432))"; then
+        echo -e "\e[34m >>> Postgres ready \e[97m"
+        break
+    fi
     sleep 1
 done
+
 
 echo -e "\e[34m >>> Starting worker \e[97m"
 python manage.py rqworker default
