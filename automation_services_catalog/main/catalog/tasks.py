e7a7982cf19a2f44fccb411189d05dca6393b66f
Madhu Kanoor <mkanoor@redhat.com>
2022-02-11 18:00:15 -0500

    Add share_count to Portfolio object
    Allows the UI to display shared portfolios

--- automation_services_catalog/main/catalog/tasks.py
+++ automation_services_catalog/main/catalog/tasks.py
@@ -1,17 +1,37 @@
+import logging
+from django.db import transaction
+
 from automation_services_catalog.main.common.tasks import (
     add_group_permissions,
     remove_group_permissions,
 )
 from automation_services_catalog.main.catalog.models import Portfolio
 
+logger = logging.getLogger("catalog")
+
 
 def add_portfolio_permissions(portfolio_id, groups_ids, permissions):
     add_group_permissions(
         Portfolio.objects.get(id=portfolio_id), groups_ids, permissions
     )
+    _update_share_counter(portfolio_id, len(groups_ids))
 
 
 def remove_portfolio_permissions(portfolio_id, groups_ids, permissions):
     remove_group_permissions(
         Portfolio.objects.get(id=portfolio_id), groups_ids, permissions
     )
+    _update_share_counter(portfolio_id, -len(groups_ids))
+
+
+@transaction.atomic
+def _update_share_counter(portfolio_id, count):
+    """Increment or decrement the share count."""
+    obj = Portfolio.objects.select_for_update().get(id=portfolio_id)
+    if obj:
+        obj.share_count += count
+        obj.save()
+    else:
+        logger.error(
+            "Portfolio {portfolio_id} not found", portfolio_id=portfolio_id
+        )
---
+++ automation_services_catalog/main/catalog/tests/test_tasks.py
@@ -0,0 +1,48 @@
+"""Test catalog tasks to add/remove permissions"""
+import pytest
+
+from automation_services_catalog.main.catalog.tests.factories import (
+    PortfolioFactory,
+)
+from automation_services_catalog.main.tests.factories import (
+    TenantFactory,
+    UserFactory,
+)
+from automation_services_catalog.main.catalog.tasks import (
+    add_portfolio_permissions,
+    remove_portfolio_permissions,
+)
+
+
+@pytest.mark.django_db
+def test_add_portfolio_permissions(mocker):
+    """Test adding portfolio permissions"""
+    tenant = TenantFactory()
+    user = UserFactory(first_name="John", last_name="Doe")
+    portfolio = PortfolioFactory(tenant=tenant, user=user)
+    group_ids = ["1", "2", "3"]
+
+    mocker.patch(
+        "automation_services_catalog.main.catalog.tasks.add_group_permissions"
+    )
+    add_portfolio_permissions(portfolio.id, group_ids, ["read"])
+    portfolio.refresh_from_db()
+    assert portfolio.share_count == len(group_ids)
+
+
+@pytest.mark.django_db
+def test_remove_portfolio_permissions(mocker):
+    """Test removing portfolio permissions"""
+    tenant = TenantFactory()
+    user = UserFactory(first_name="John", last_name="Doe")
+    group_ids = ["1", "2", "3"]
+    portfolio = PortfolioFactory(
+        tenant=tenant, user=user, share_count=len(group_ids)
+    )
+
+    mocker.patch(
+        "automation_services_catalog.main.catalog.tasks.remove_group_permissions"
+    )
+    remove_portfolio_permissions(portfolio.id, group_ids, ["read"])
+    portfolio.refresh_from_db()
+    assert portfolio.share_count == 0
