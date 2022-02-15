6df3d884169daf64ad1ad1f919fdcf14099c7d36
Madhu Kanoor <mkanoor@redhat.com>
2022-02-15 13:43:07 -0500

    imported Client
    Fixes #344

--- automation_services_catalog/main/auth/tests/functional/test_login_url.py
+++ automation_services_catalog/main/auth/tests/functional/test_login_url.py
@@ -1,11 +1,11 @@
-from django.test import Client as TestClient
+from django.test import Client
 from django.urls import reverse
 
 from rest_framework import status
 
 
 def test_login_redirect():
-    client = TestClient()
+    client = Client()
     response = client.get(reverse("auth:login"))
 
     assert response.status_code == status.HTTP_302_FOUND
