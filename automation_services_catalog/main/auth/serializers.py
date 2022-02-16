63904a674d01600a5ecaaae917c98faf9954d593
Madhu Kanoor <mkanoor@redhat.com>
2022-02-16 10:32:03 -0500

    Pylint cleanup
    Use get_user_model instead of directly refering to the User model

--- automation_services_catalog/main/auth/serializers.py
+++ automation_services_catalog/main/auth/serializers.py
@@ -1,21 +1,24 @@
+"""Serializer for auth object"""
 from rest_framework import serializers
 
 import jwt
 
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema_field
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
+    """Current user info to be sent to the caller"""
     roles = serializers.SerializerMethodField("get_roles")
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ("username", "first_name", "last_name", "roles")
 
     @extend_schema_field(field={"type": "array", "items": {"type": "string"}})
-    def get_roles(self, obj):
+    def get_roles(self, _obj):
+        """Get the current users roles from the request"""
         request = self.context.get("request")
         extra_data = request.keycloak_user.extra_data
         jot = jwt.decode(
