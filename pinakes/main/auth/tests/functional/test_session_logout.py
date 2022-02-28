ed113d4778b2ee966c81740a1663e2c2da4106f8
Madhu Kanoor <mkanoor@redhat.com>
2022-02-28 14:23:51 -0500

    Fix test failure
    PR https://github.com/ansible/pinakes/pull/381 introduced changes
    for SSL which caused the tests to fail.

--- pinakes/main/auth/tests/functional/test_session_logout.py
+++ pinakes/main/auth/tests/functional/test_session_logout.py
@@ -5,6 +5,6 @@ import pytest
 @pytest.mark.django_db
 def test_session_logout(api_request, mocker):
     """Logout an authenticated user from a single session"""
-    mocker.patch("pinakes.main.auth.views.OpenIdConnect")
+    mocker.patch("pinakes.main.auth.views.get_oidc_client")
     response = api_request("post", "auth:logout")
     assert response.status_code == 200
