f3b4e116f930da449abc807ac017ebc61cd0f0f6
Alejandro Izquierdo <aizquier@redhat.com>
2022-03-01 10:10:25 +0100

    fix keycloak setup errors and move ansible collection

--- tools/docker/scripts/keycloak-setup.sh
+++ tools/docker/scripts/keycloak-setup.sh
@@ -3,11 +3,12 @@ set -e
 
 while true;
 do echo "Waiting for keycloak service to come up...";
-    wget --no-check-certificate $PINAKES_KEYCLOAK_URL -q -T 1 -O /dev/null >/dev/null 2>/dev/null && break;
+    wget --no-check-certificate $PINAKES_KEYCLOAK_URL/ -q -T 1 -O /dev/null >/dev/null 2>/dev/null && break;
     sleep 3;
 done;
 echo "Service is up!"
 
 echo -e "\e[34m >>> Installing keycloak setup collections \e[97m"
 
-ansible-playbook -vvv tools/keycloak_setup/dev.yml
+cd tools/keycloak_setup
+ansible-playbook pinakes.keycloak.dev -vvv
---
+++ tools/keycloak_setup/ansible_collections/pinakes/keycloak/README.md
@@ -0,0 +1,13 @@
+# Ansible Collection - pinakes_keycloak
+
+This collection allows you to configure Keycloak for
+pinakes project.
+
+It adds
+
+- Roles
+- Policies
+- Scopes
+- Permissions
+- Groups (optional)
+- Users (optional)
