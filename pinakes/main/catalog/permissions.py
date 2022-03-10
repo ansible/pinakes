bc94af62d0d05be977bbce3aecb7fce4ab8a914f
Alexander Saprykin <cutwatercore@gmail.com>
2022-03-10 11:36:07 +0100

    Implement portfolio item permission class

--- pinakes/main/catalog/permissions.py
+++ pinakes/main/catalog/permissions.py
@@ -13,11 +13,13 @@ from pinakes.common.auth.keycloak_django.permissions import (
     check_resource_permission,
     get_permitted_resources,
 )
-from pinakes.main.catalog.models import Portfolio
+from pinakes.main.catalog.models import (
+    Portfolio,
+    PortfolioItem,
+)
 
 
 class PortfolioPermission(BaseKeycloakPermission):
-
     access_policies = {
         "list": KeycloakPolicy("read", KeycloakPolicy.Type.QUERYSET),
         "retrieve": KeycloakPolicy("read", KeycloakPolicy.Type.OBJECT),
@@ -80,3 +82,66 @@ class PortfolioPermission(BaseKeycloakPermission):
             return qs
         else:
             return qs.filter(pk__in=resources.items)
+
+
+class PortfolioItemPermission(BaseKeycloakPermission):
+    access_policies = {
+        "retrieve": KeycloakPolicy("read", KeycloakPolicy.Type.OBJECT),
+        "create": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "update": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "partial_update": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "destroy": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        # Tags
+        "tags": KeycloakPolicy("read", KeycloakPolicy.Type.OBJECT),
+        "tag": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "untag": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        # Custom actions
+        "icon": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "copy": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "next_name": KeycloakPolicy("read", KeycloakPolicy.Type.OBJECT),
+    }
+
+    def get_access_policies(self, request: Request, view: Any):
+        if "portfolio_id" in view.kwargs:
+            policy_type = KeycloakPolicy.Type.OBJECT
+        else:
+            policy_type = KeycloakPolicy.Type.QUERYSET
+        return {
+            **self.access_policies,
+            "list": KeycloakPolicy("read", policy_type),
+        }
+
+    def perform_check_object_permission(
+        self,
+        permission: str,
+        request: Request,
+        view: Any,
+        obj: Any,
+    ) -> bool:
+        if isinstance(obj, PortfolioItem):
+            obj = obj.portfolio
+        elif not isinstance(obj, Portfolio):
+            return False
+        return check_resource_permission(
+            obj.keycloak_type(),
+            obj.keycloak_name(),
+            permission,
+            get_authz_client(request.keycloak_user.access_token),
+        )
+
+    def perform_scope_queryset(
+        self,
+        permission: str,
+        request: Request,
+        view: Any,
+        qs: models.QuerySet,
+    ) -> models.QuerySet:
+        resources = get_permitted_resources(
+            Portfolio.keycloak_type(),
+            permission,
+            get_authz_client(request.keycloak_user.access_token),
+        )
+        if resources.is_wildcard:
+            return qs
+        else:
+            return qs.filter(portfolio__in=resources.items)
