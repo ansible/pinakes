f0e5b164c6df80c16211e48f2715266859594721
Madhu Kanoor <mkanoor@redhat.com>
2022-03-15 13:40:32 -0400

    Fixed Order create permission issue
    1. Any logged in user should be able to create an order object
    2. The submit/cancel are part of catalog:order:update permission

--- pinakes/main/catalog/permissions.py
+++ pinakes/main/catalog/permissions.py
@@ -151,18 +151,15 @@ class OrderPermission(BaseKeycloakPermission):
         "destroy": KeycloakPolicy("delete", KeycloakPolicy.Type.OBJECT),
         # Custom actions
         # TODO(cutwater): Define policies for `submit` and `cancel` actions.
-        "submit": KeycloakPolicy("order", KeycloakPolicy.Type.OBJECT),
-        "cancel": KeycloakPolicy("order", KeycloakPolicy.Type.OBJECT),
+        "submit": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
+        "cancel": KeycloakPolicy("update", KeycloakPolicy.Type.OBJECT),
     }
 
     def perform_check_permission(
         self, permission: str, request: Request, view: Any
     ) -> bool:
-        return check_wildcard_permission(
-            Order.keycloak_type(),
-            permission,
-            get_authz_client(request.keycloak_user.access_token),
-        )
+        # If the user is authenticated they are allowed to create an order
+        return True
 
     def perform_check_object_permission(
         self,
