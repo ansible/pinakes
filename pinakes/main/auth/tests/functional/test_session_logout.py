3c3042c5079376fd1479c1fb0a2556586d5bf2ac
Alexander Saprykin <cutwatercore@gmail.com>
2022-04-19 16:56:32 +0200

    [SSP-2879] Fix DRF browser for SessionLogoutView
    The exception is caused by `get_serializer()` method override,
    returning None.
    Removing the override doesn't affect OpenAPI schema generation.
    
    https://issues.redhat.com/browse/SSP-2879

--- pinakes/main/auth/tests/functional/test_session_logout.py
+++ pinakes/main/auth/tests/functional/test_session_logout.py
@@ -1,5 +1,6 @@
-""" Module to test session logout end point """
+"""Module to test session logout end point."""
 import pytest
+from rest_framework import status
 
 
 @pytest.mark.django_db
@@ -7,4 +8,4 @@ def test_session_logout(api_request, mocker):
     """Logout an authenticated user from a single session"""
     mocker.patch("pinakes.main.auth.views.get_oidc_client")
     response = api_request("post", "auth:logout")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_204_NO_CONTENT
