5e150b154261ec49d1fa22f07acf0aaaf2d42302
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 21:18:58 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -1,6 +1,10 @@
 # Change Directory to clones
 cd _clones
 
+# Install Python Packages & activate virtual env
+python3 -m venv extract-strings-env
+pip3 install requirements.txt
+
 # Extract API Strings
 source extract-strings-env/bin/activate && python3 manage.py makemessages -l en --ignore "extract-strings-env/*"
 
