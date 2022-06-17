e350c035acb98530ec75d04f5ffbb556b841d438
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 21:32:09 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -11,3 +11,5 @@ source extract-strings-env/bin/activate && python3 manage.py makemessages -l en 
 
 # Move files to translations folder
 mv pinakes/locale/en/LC_MESSAGES/django.po translations/
+
+sleep 10
